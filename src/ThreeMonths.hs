0267af55dbf29e90feb75df33bbba205094c3d2b
Rashad Gover <rashad.sasaki@gmail.com>
2022-09-06 00:24:31 +0000

    Implement calendar for module A

--- src/ThreeMonths.hs
+++ src/ThreeMonths.hs
@@ -73,11 +73,11 @@ firstMonth = Month week1 week2 week3 week4
 
     week4 =
       Week
-        (Day [] "" "")
-        (Day [] "" "")
-        (Day [] "" "")
-        (Day [] "" "")
-        (Day [] "" "")
+        (Day ["PROJECT: Refactor CLI App into Web App"] "" "")
+        (Day ["PROJECT: Refactor CLI App into Web App"] "" "")
+        (Day ["PROJECT: Refactor CLI App into Web App"] "" "")
+        (Day ["PROJECT: Refactor CLI App into Web App"] "" "")
+        (Day ["PROJECT: Refactor CLI App into Web App"] "" "")
         (Day [] "" "")
         (Day [] "" "")
 
@@ -105,7 +105,7 @@ homeHandler = do
     calendar =
       [Perl6.qq|
             <!-- This example requires Tailwind CSS v2.0+ -->
-            <div class="lg:flex lg:h-full lg:flex-col">
+            <div class="lg:flex lg:h-full lg:flex-col md:m-5 lg:m-10">
                 $calendarHeader
                 $calendarDays
                 $dayEventList
@@ -194,7 +194,7 @@ homeHandler = do
       [Perl6.q|
             <header class="flex items-center justify-between border-b border-gray-200 py-4 px-6 lg:flex-none">
                 <h1 class="text-lg font-semibold text-gray-900">
-                    <time datetime="2022-01">January 2022</time>
+                    <time datetime="2022-01">Module A: Pure Functions & IO</time>
                 </h1>
                 <div class="flex items-center">
                     <div class="flex items-center rounded-md shadow-sm md:items-stretch">
@@ -210,7 +210,7 @@ homeHandler = do
                             </svg>
                         </button>
                         <button type="button"
-                            class="hidden border-t border-b border-gray-300 bg-white px-3.5 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:relative md:block">Today</button>
+                            class="hidden border-t border-b border-gray-300 bg-white px-3.5 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:relative md:block">Module A</button>
                         <span class="relative -mx-px h-5 w-px bg-gray-300 md:hidden"></span>
                         <button type="button"
                             class="flex items-center justify-center rounded-r-md border border-l-0 border-gray-300 bg-white py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50">
@@ -223,49 +223,6 @@ homeHandler = do
                                     clip-rule="evenodd" />
                             </svg>
                         </button>
-                    </div>
-                    <div class="hidden md:ml-4 md:flex md:items-center">
-                        <div class="relative">
-                            <button type="button"
-                                class="flex items-center rounded-md border border-gray-300 bg-white py-2 pl-3 pr-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
-                                id="menu-button" aria-expanded="false" aria-haspopup="true">
-                                Full-time
-                                <!-- Heroicon name: mini/chevron-down -->
-                                <svg class="ml-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </button>
-
-                            <!--
-                    Dropdown menu, show/hide based on menu state.
-        
-                    Entering: "transition ease-out duration-100"
-                        From: "transform opacity-0 scale-95"
-                        To: "transform opacity-100 scale-100"
-                    Leaving: "transition ease-in duration-75"
-                        From: "transform opacity-100 scale-100"
-                        To: "transform opacity-0 scale-95"
-                    -->
-                            <div class="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                                <div class="py-1" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-                                        id="menu-item-0">Part-time</a>
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-                                        id="menu-item-1">Full-time</a>
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-                                        id="menu-item-2">Overtime</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="ml-6 h-6 w-px bg-gray-300"></div>
-                        <button type="button"
-                            class="ml-6 rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add
-                            event</button>
                     </div>
                     <div class="relative ml-6 md:hidden">
                         <button type="button"
@@ -321,7 +278,6 @@ homeHandler = do
     calendarDays =
       [Perl6.qq|
                 <div class="shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
-                        $calendarDaysSR
                         {calendarDaysFull firstMonth}
                         $calendarDaysMobile
                     </div>
